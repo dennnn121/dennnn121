535db2710dc80396d8a5fd0c54901726ec817655
dennnn121 <denirahmadana63@gmail.com>
2024-09-23 19:06:49 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('signupForm');
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        alert('Form telah dikirim!');
-        form.reset();
-    });
-});
